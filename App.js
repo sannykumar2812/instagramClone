d0930925370232c84f388dbc16dd7a7981e3d182
Sanny kumar <sunnykusingh2812@gmail.com>
2022-03-03 15:28:35 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+    StatusBar,
+    SafeAreaView
+} from 'react-native';
+
+import HomeScreen from "./src/screens/HomeScreen"
+
+
+const App = () =>{
+    return (
+        <>
+            <StatusBar barStyle={barStyle='dark-content'} /><SafeAreaView>
+                <HomeScreen />
+            </SafeAreaView>
+        </>
+    )
+}
+
+
+export default App;
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+import {Text,View} from "react-native";
+
+
+const HomeScreen = () => {
+    return(
+        <Text>sanny here!!</Text>
+    )
+};
+
+export default HomeScreen;
